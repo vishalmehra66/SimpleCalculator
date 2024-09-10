3f3c93a5805eb732b28d17530cf2a363691540aa
vishalmehra66 <vishalbairwa0450@gmail.com>
2024-09-10 17:05:06 +0530

    first commit

---
+++ calculator/calculator.js
@@ -0,0 +1,15 @@
+
+
+function display(val){
+  document.getElementById("result").value+=val;
+}
+
+function equate(){
+    let x = document.getElementById("result").value;
+    let y = eval(x);
+    document.getElementById("result").value = y;
+}
+
+function clr(){
+    document.getElementById("result").value = "";
+}
